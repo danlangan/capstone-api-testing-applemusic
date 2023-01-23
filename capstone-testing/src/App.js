7a00d92d65ae800e90433cdea2777fbe17ecbc28
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2023-01-23 17:33:36 -0600

    added the code to the repo

--- capstone-testing/src/App.js
+++ capstone-testing/src/App.js
@@ -1,10 +1,41 @@
 import React from "react";
-
+// import '.../AuthKey_Y8F8JV7CXD';
 
 function App() {
-  return (
-    <div className="App">
 
+  const jwt = require('jsonwebtoken');
+  const fs = require('fs');
+
+  let now = new Date();
+  let nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate());
+  let nowEpoch = Math.round(now.getTime() / 1000); // number of seconds since Epoch, in UTC
+  let nextMonthEpoch = Math.round(nextMonth.getTime() / 1000); // number of seconds since Epoch, in UTC
+  
+  const privateKey = fs.readFileSync('../AuthKey_Y8F8JV7CXD.p8');
+
+  const payload = { 
+    iss: 'A4NXNNBMQ6',
+    iat: nowEpoch,
+    exp: nextMonthEpoch
+  };
+
+  const options = {
+    algorithm: 'ES256',
+    header: {
+      alg: 'ES256',
+      kid: 'Y8F8JV7CXD' // KEY ID
+    }
+  };
+  
+  jwt.sign(payload, privateKey, options, function(error, token) {
+    console.log(error);
+    console.log(token);
+  });
+
+  return (
+      
+    <div className="App">
+      this should work
     </div>
   );
 }
